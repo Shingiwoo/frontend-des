7256fcb0249e8ffe9725f510faaf727d0adef4c9
shingiwoo <shingiwoo.ind@gmail.com>
2024-10-19 23:22:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import routes
+import Routes from './routes';
+
+//import Toaster
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+
+  return (
+    <>
+      <Routes />
+      <Toaster />
+
+    </>
+  )
+}
+
+export default App
